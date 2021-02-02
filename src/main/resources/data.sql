c0183ddd9c90c07e9f297551d4cee09bb332bed7
karice <kl529@naver.com>
2021-02-02 17:50:00 +0900

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into user values(9001,'우리집1','aaaa@naver.com', 'AAAAA', '01011111111');
+insert into user values(9002,'우리집2','bbbb@naver.com', 'BBBBB', '01022222222');
+insert into user values(9003,'우리집3','cccc@naver.com', 'CCCCC', '01033333333');
+
+-- insert into post values(10001, 'my First Post', 9001);
+-- insert into post values(10002, 'my second Post', 9001);
---
+++ src/test/java/com/shopping/shoppingmallcustomer/ShoppingMallCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopping.shoppingmallcustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMallCustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
