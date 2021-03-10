ee91e919abf30248450636911cccbeeb2c5931ac
karice <kl529@naver.com>
2021-03-10 14:42:49 +0900

    [Create] No_DB create

--- src/main/java/com/shopping/customer/customer/customer/CustomerController.java
+++ src/main/java/com/shopping/customer/customer/customer/CustomerController.java
@@ -9,16 +9,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("")
 public class CustomerController {
 
     GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("data.xml");
     Customer user = ctx.getBean("resturant1", Customer.class);
 
+    @GetMapping("")
+    public String test(){
+        return "Hello";
+    }
+
     @GetMapping("/{id}")
     public JSONObject retrieveProduct(@PathVariable int id) throws ParseException {
         String output = "{" + "name:" + user.getNametoid(id) + ", phone_number:" + user.getPhone_numbertoid(id) + ", address:" + user.getAddresstoid(id) + "}";
 
-        System.out.println(output);
+//        System.out.println(output);
         JSONParser parser = new JSONParser();
         Object obj = parser.parse(output);
         JSONObject jsonObj = (JSONObject) obj;
