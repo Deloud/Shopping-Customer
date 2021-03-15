d15ba91ce9472ccfc4ec3b3d125d33f8e2bf17db
herjn <leejungin199@naver.com>
2021-03-15 20:43:41 +0900

    [Update] delete files

--- src/main/java/com/shopping/shoppingmallcustomer/customer/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.shopping.shoppingmallcustomer.customer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-insert into user values(9001,'우리집1','aaaa@naver.com', 'AAAAA', '01011111111');
-insert into user values(9002,'우리집2','bbbb@naver.com', 'BBBBB', '01022222222');
-insert into user values(9003,'우리집3','cccc@naver.com', 'CCCCC', '01033333333');
-
--- http://localhost:8089/swagger-ui.html#/ --> SWAGGER
--- http://localhost:8088/h2-console/ --> H2
